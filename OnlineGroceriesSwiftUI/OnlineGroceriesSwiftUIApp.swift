a242054d77b2134393c97827218ef3e6bcded1cd
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-30 18:01:44 +0200

    Initial Commit

---
+++ OnlineGroceriesSwiftUI/OnlineGroceriesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesSwiftUIApp.swift
+//  OnlineGroceriesSwiftUI
+//
+//  Created by Manyuchi, Carrington C on 2025/05/30.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
